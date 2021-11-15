002a3fbc09f4435f1fb755ff55db1519ef07e5f7
Loïc <l.vandenbossche.jouvet@gmail.com>
2021-11-15 21:35:37 +0100

    Added SDL2 library support and main function

---
+++ SDL-version/headers/main.h
@@ -0,0 +1,13 @@
+//
+// Created by Loïc Vanden Bossche on 15/11/2021.
+//
+
+#define SDL_MAIN_HANDLED
+
+#ifndef MALLOC_WORLD_MAIN_H
+#define MALLOC_WORLD_MAIN_H
+
+#include "stdio.h"
+#include <SDL2/SDL.h>
+
+#endif //MALLOC_WORLD_MAIN_H
