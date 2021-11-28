7fd93f70afa503dfbc83261beabc6731c3495d48
Loïc <l.vandenbossche.jouvet@gmail.com>
2021-11-28 11:47:44 +0100

    Added main display

--- console-version/src/display.c
+++ console-version/src/display.c
@@ -23,7 +23,7 @@ void getMapDisplayOffsetParams(coordinate playerPos, int* sXParam, int* sYParam,
         *sXParam = 0;
         *eXParam = DISPLAY_MAP_SIZE_X;
     } else if (playerPos.x > MAP_SIZE_X - (DISPLAY_MAP_SIZE_X/2)) {
-        *sXParam = MAP_SIZE_X - DISPLAY_MAP_SIZE_X;
+        *sXParam = MAP_SIZE_X - DISPLAY_MAP_SIZE_X - 1;
         *eXParam = MAP_SIZE_X;
     } else {
         *sXParam = playerPos.x - (DISPLAY_MAP_SIZE_X/2);
@@ -34,7 +34,7 @@ void getMapDisplayOffsetParams(coordinate playerPos, int* sXParam, int* sYParam,
         *sYParam = 0;
         *eYParam = DISPLAY_MAP_SIZE_Y;
     } else if (playerPos.y > MAP_SIZE_Y - (DISPLAY_MAP_SIZE_Y/2)) {
-        *sYParam = MAP_SIZE_Y - DISPLAY_MAP_SIZE_Y;
+        *sYParam = MAP_SIZE_Y - DISPLAY_MAP_SIZE_Y - 1;
         *eYParam = MAP_SIZE_Y;
     } else {
         *sYParam = playerPos.y - (DISPLAY_MAP_SIZE_Y/2);
@@ -42,37 +42,45 @@ void getMapDisplayOffsetParams(coordinate playerPos, int* sXParam, int* sYParam,
     }
 }
 
-void displayGrid(int** grid, coordinate playerPos) {
+void displayGridCoords(int** grid, coordinate coords) {
+
+    switch(grid[coords.y][coords.x]) {
+        case TILE_WALL: isUnicode ? printf("\uE191") : putchar('#'); break;
+        case TILE_FLOOR: putchar(' '); break;
+        case -3 ... -2: isUnicode ? printf("\uE6DC") : putchar('S'); break;
+        case 1: isUnicode ? printf("\uE0E8") : putchar('1'); break;
+        case 2: isUnicode ? printf("\uE2E6") : putchar('2'); break;
+        case 3:
+        case 6:
+        case 9: isUnicode ? printf("\uE86A") : putchar('P'); break;
+        case 4:
+        case 7:
+        case 10: isUnicode ? printf("\uE52A") : putchar('R'); break;
+        case 5:
+        case 8:
+        case 11: isUnicode ? printf("\uE9B5") : putchar('A'); break;
+        case 12 ... 40: isUnicode ? printf("\uE85E") : putchar('M'); break;
+        case 41 ... 69: isUnicode ? printf("\uE82B") : putchar('M'); break;
+        case 70 ... 98: isUnicode ? printf("\uE8A7") : putchar('M'); break;
+        case 99: isUnicode ? printf("\uE89A") : putchar('B'); break;
+        default: printf("%d", grid[coords.y][coords.x]); break;
+    }
+}
+
+void display(map* worldMap) {
 
     int sXParam, sYParam, eXParam, eYParam;
 
-    getMapDisplayOffsetParams(playerPos, &sXParam, &sYParam, &eXParam, &eYParam);
+    getMapDisplayOffsetParams(worldMap->currentCoords, &sXParam, &sYParam, &eXParam, &eYParam);
 
-    int xi, yi;
-    for(yi=sYParam; yi<eYParam; yi++)
-    {
-        for(xi=sXParam; xi<eXParam; xi++)
-        {
-            switch(grid[yi][xi]) {
-                case TILE_WALL: isUnicode ? printf("\uE191") : putchar('#'); break;
-                case TILE_FLOOR: putchar(' '); break;
-                case -3 ... -2: isUnicode ? printf("\uE6DC") : putchar('S'); break;
-                case 1: isUnicode ? printf("\uE0E8") : putchar('1'); break;
-                case 2: isUnicode ? printf("\uE2E6") : putchar('2'); break;
-                case 3:
-                case 6:
-                case 9: isUnicode ? printf("\uE86A") : putchar('P'); break;
-                case 4:
-                case 7:
-                case 10: isUnicode ? printf("\uE52A") : putchar('R'); break;
-                case 5:
-                case 8:
-                case 11: isUnicode ? printf("\uE9B5") : putchar('A'); break;
-                case 12 ... 40: isUnicode ? printf("\uE85E") : putchar('M'); break;
-                case 41 ... 69: isUnicode ? printf("\uE82B") : putchar('M'); break;
-                case 70 ... 98: isUnicode ? printf("\uE8A7") : putchar('M'); break;
-                case 99: isUnicode ? printf("\uE89A") : putchar('B'); break;
-                default: printf("%d", grid[yi][xi]); break;
+    for (int dy = 0; dy < DISPLAY_SIZE_Y; ++dy) {
+        for (int dx = 0; dx < DISPLAY_SIZE_X; ++dx) {
+            if(dy < DISPLAY_MAP_SIZE_Y && dx < DISPLAY_MAP_SIZE_X) {
+
+                displayGridCoords(worldMap->lvl[worldMap->currentLvl], (coordinate){ sXParam + dx, sYParam + dy });
+
+            } else {
+                printf(".");
             }
         }
         putchar('\n');
@@ -80,6 +88,5 @@ void displayGrid(int** grid, coordinate playerPos) {
 }
 
 void displayMap(map* worldMap) {
-
-    displayGrid(worldMap->lvl[worldMap->currentLvl], worldMap->currentCoords);
+    display(worldMap);
 }
